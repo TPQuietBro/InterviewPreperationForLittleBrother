96cb9f0dd77dfc58125f2cb8f41745eeefa3cfd7
TPQuietBro <1186886308@qq.com>
2019-09-03 17:12:40 +0800

    TwoSum

---
+++ Algorithm/LeetCode/TwoSum.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       let result = twoSum(numbers: [2, 7, 11, 15,3,10,8],target: 18)
+        print("\(result)")
+    }
+    
+    func twoSum(numbers:[Int],target:Int)->[[Int]]{
+        if (numbers.count == 0) return [];
+        var result = [[Int]]()
+        var numMap = [Int : Int]()
+        for i in 0..<numbers.count{
+            let another = target - numbers[i]
+            let targetNumber = numMap[another]
+            if (targetNumber != nil){
+                result.append([targetNumber!,i])
+            }
+            numMap[numbers[i]] = i;
+        }
+        return result;
+    }
+
+}
